021ab97933a699305f90098d3101320d2db3ee1a
itm22 <itm22@dongyang.ac.kr>
2025-12-03 11:14:58 +0900

    Add files via upload

---
+++ 자바_과제/src/Object_class_2/Member.java
@@ -0,0 +1,5 @@
+package Object_class_2;
+
+public record Member(String id, String name, int age) {
+
+}
---
+++ 자바_과제/src/사용자_정의_예외/Account.java
@@ -0,0 +1,25 @@
+package 사용자_정의_예외;
+
+public class Account {
+
+	private long balance;
+	
+	public Account() {
+		
+	}
+	
+	public long getBalance() {
+		return balance;
+	}
+	
+	public void deposit(int money) {
+		balance += money;
+	}
+	
+	public void withdraw(int money) throws InsufficientException {
+		if (balance < money) {
+			throw new InsufficientException ("잔고부족: " + (money-balance) + " 모자람");
+		}
+		balance -= money; 
+	}
+}
